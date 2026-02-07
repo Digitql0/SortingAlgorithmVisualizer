05ce767a13a65f55a271c897398e5c6a7b8eac80
Digitql0 <imonkix@gmail.com>
2026-02-07 16:52:25 +0100

    coupled sorters in menu with their shape

--- C++/include/menu.hpp
+++ C++/include/menu.hpp
@@ -5,6 +5,12 @@
 #include <optional>
 #include <raylib.h>
 #include <vector>
+
+struct MenuHelper {
+    Sorter sorter;
+    Rect rect;
+}
+
 class Menu {
 public:
   void addSorter(Sorter sorter);
@@ -20,11 +26,11 @@ private:
   bool static calculateEndCondition();
   bool (*end_condition_func)() = calculateEndCondition;
   void drawMenu();
-  std::vector<Sorter> options;
+  std::vector<MenuHelper> options;
   unsigned int rows;
   unsigned int columns;
   bool end_condition = false;
-  std::optional<Sorter> curr;
+  std::optional<MenuHelper> curr;
   void (*draw)(Sorter &sorter);
 };
 
